e1530e203c651c3afd494d4b8d5ac61ecfa73675
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-10-15 22:14:10 +0500

    feat: setup

--- Sources/ACTestPackage/ACTestPackage.swift
+++ Sources/ACTestPackage/ACTestPackage.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public class TestClass {
+
+    public init() { }
+
+    public func test() {
+        print("[TestClass] called from ACTestPacakge")
+    }
+}
